4f7df7f3c778578dbb76937f5991e14b98339507
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-01-11 09:06:44 +0100

    Initial project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import "../assets/styles/App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Kometa App! &#x2604;&#xFE0F;</h1>
+        </div>
+    );
+}
+
+export default App;
