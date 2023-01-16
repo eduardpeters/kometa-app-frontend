c2824ba0e6fccd4d36968f902f69f5f3535401b0
laurapellicerrahan <laura.pellicer.rahan@gmail.com>
2023-01-16 21:10:29 +0100

    router & navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import "../assets/styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <h1>Kometa App! &#x2604;&#xFE0F;</h1>
+            <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
         </div>
     );
 }
