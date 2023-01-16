4aa566c149a6a1bd27e630b08f155b351484f591
laurapellicerrahan <laura.pellicer.rahan@gmail.com>
2023-01-16 21:27:07 +0100

    logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,21 @@
 import "../assets/styles/App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+/* import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; */
+import Navbar from "./navbar/Navbar.jsx";
 
 function App() {
     return (
         <div className="App">
             <h1>Kometa App! &#x2604;&#xFE0F;</h1>
-            <Router>
             <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </Router>
         </div>
     );
 }
 
 export default App;
+
+/* <Router>
+<Navbar />
+<Routes>
+  <Route path="/" element={<Home />} />
+</Routes>
+</Router> */
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './logo-kometa.png'
+
+const navbar = () => {
+  return (
+    <div>
+        <img src={logo} alt="Logo" />
+    </div>
+  )
+}
+
+export default navbar
