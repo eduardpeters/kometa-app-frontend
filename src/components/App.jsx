ca9c6a292673a85a6fca8b934ed7a198711f7008
rocio lanfranconi <rociolanfranconi@gmail.com>
2023-01-16 20:55:23 +0100

    add dev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import "../assets/styles/App.css";
 function App() {
     return (
         <div className="App">
-            <h1>Kometa App! &#x2604;&#xFE0F;</h1>
+            <h1>Kometa App!!! &#x2604;&#xFE0F;</h1>
         </div>
     );
 }
