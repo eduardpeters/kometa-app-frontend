afc5fa33d285875b177a50bcbe399722443b67c3
laurapellicerrahan <laura.pellicer.rahan@gmail.com>
2023-01-17 18:08:18 +0100

    Merge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import "../assets/styles/App.css";
 /* import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; */
 import Navbar from "./navbar/Navbar.jsx";
 import Search from "./search/Search.jsx";
+import SelectorUser from "./selector-user/SelectorUser";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <SelectorUser />
             <Search />
         </div>
     );
