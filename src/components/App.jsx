2327973b1ff8750e96c2c27b1ee6c978310d3478
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-02-04 11:15:01 +0100

    Reorder login.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,16 +8,13 @@ import Login from "./login/Login";
 import Form from "../components/form/Form.jsx"
 import LandingPage from "./landingpage/LandingPage";
 
-/* import Formulary from '../Pages/Formulary';
-import LandingPage from '../Pages/LandinPage'; */
-
 function App() {
 
     return (
         <div className="App">
             <Router>
                 <Routes>
-                    <Route path="/" element={<LandingPage/>} />
+                    <Route path="/" element={<LandingPage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Form />} />
                     <Route path="/no-order" element={<NoOrder />} />
