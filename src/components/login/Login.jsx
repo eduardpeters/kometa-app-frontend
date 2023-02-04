ddef64d326e24e95bb7ab59992e60394777ea0ab
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-02-04 09:21:02 +0100

    Login Form refactoring

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../../assets/styles/login.css'
 import image from '../../assets/Images/repartidorlogin.png'
 import NavLogin from "../../components/nav-login/NavLogin.jsx";
-import Register from "../register/Register";
 // import Googlebtn from '../components/Googlebtn'
 
 function Login() {
@@ -12,11 +11,10 @@ function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const navigate = useNavigate();
-
-    const navigateToFormlogin = () => {
-        navigate('/register');
-    };
+    async function handleLoginSubmit(event) {
+        event.preventDefault();
+        console.log("Post Login!");
+    }
 
     return (
         <section className="login">
@@ -27,35 +25,40 @@ function Login() {
                 <div className="login-navbar">
                     <NavLogin />
                 </div>
-                <form class="login-form" >
+                <form class="login-form" onSubmit={handleLoginSubmit}>
                     <div className='form-title'>
                         <h2 className='title-log-in'>Inicio de sesión</h2>
                         <h4 className='subtitle-login'>Introduce tus datos para iniciar sesión</h4>
                     </div>
                     <div className="login-inputs">
                         <div className="login-field">
-                            <h3 className='title-input'>Email</h3>
-                            <input className='input' value={email} onChange={(e) => setEmail(e.target.value)} type="text" required placeholder='kometa@kometa.com' />
+                            <label htmlFor="email" className='title-input'>Email</label>
+                            <input className='input' value={email} onChange={(e) => setEmail(e.target.value)} type="text" required placeholder='kometa@kometa.com' id="email" />
                         </div>
                         <div className="login-field">
-                            <h3 className='title-input'>Contraseña</h3>
-                            <input className='input' value={password} onChange={(e) => setPassword(e.target.value)} type="text" required placeholder='*************' />
+                            <label htmlFor="password" className='title-input'>Contraseña</label>
+                            <input className='input' value={password} onChange={(e) => setPassword(e.target.value)} type="password" required placeholder='*************' id="password" />
                         </div>
-                        <div className="w-33">
-                            <div className="center1">
-                                <Register />
-                                <button className='btn-sesion'>Inicio de sesión</button>
+                        <div className="center4">
+                            <div class="check-container">
+                                <input className="checkbox" type="checkbox" id="remember" />
+                                <label htmlFor="remember" className="slider"> Recuérdame</label>
                             </div>
                         </div>
+                        <button className='btn-login' type="submit">Inicio de sesión</button>
                     </div>
                 </form>
                 {/* <Googlebtn /> */}
                 <div className="box-register">
-                    <h4 className='subtitle-log-in' onClick={navigateToFormlogin}>¿No tienes cuenta? <b>Registrate</b></h4>
+                    <h4 className='subtitle-log-in' >
+                        <Link to="/register" className="text-link">
+                            ¿No tienes cuenta? <b>Registrate</b>
+                        </Link>
+                    </h4>
                 </div>
             </div>
         </section>
     )
 }
 
-export default Login
+export default Login;
