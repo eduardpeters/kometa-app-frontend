609710bbebfcb386b4009684b41652602e244760
laurapellicerrahan <laura.pellicer.rahan@gmail.com>
2023-02-12 13:29:21 +0100

    Card History User

--- src/components/history-user/CardHistoryUser.jsx
+++ src/components/history-user/CardHistoryUser.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import '../../assets/styles/history.css'
+import BtnInDeliveryStatus from '../buttons/btns-status/BtnInDeliveryStatus'
 import BtnCompletedStatus from '../buttons/btns-status/BtnCompletedStatus'
 import InfoCardDestination from '../info-cards/InfoCardDestination'
 import InfoCardOrigin from '../info-cards/InfoCardOrigin'
 import InfoDeliveryMan from '../info-cards/InfoDeliveryMan'
+import { useState } from 'react'
 
-const HistoryCompletedUser = () => {
+const CardHistoryUser = () => {
+
+    const [status, setStatus] = useState(false);
+
   return (
     <div className='bg-component-history'>
       <InfoDeliveryMan />
@@ -13,9 +18,9 @@ const HistoryCompletedUser = () => {
       <div className='details-history'>9€</div>
       <InfoCardOrigin />
       <InfoCardDestination />
-      <BtnCompletedStatus />
+      {status ? <BtnInDeliveryStatus /> : <BtnCompletedStatus />}
     </div>
   )
 }
 
-export default HistoryCompletedUser
+export default CardHistoryUser
