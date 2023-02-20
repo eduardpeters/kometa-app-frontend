dcfd3d4a291abc63b241f5e01bfe37f1ceb901ce
laurapellicerrahan <laura.pellicer.rahan@gmail.com>
2023-02-20 11:30:30 +0100

    Obtain information from the CSV file

--- src/components/create-order/PopupUploadFile.jsx
+++ src/components/create-order/PopupUploadFile.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
 import '../../assets/styles/popup-upload-file.css'
+import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import PaPa from 'papaparse';
 
 const PopupUploadFile = (props) => {
 
     const navigate = useNavigate();
+    const file = useRef();
 
-    const navigateToCreateOrder = () => {
-        navigate('/create-order');
+    const handleFileUpload = () => {
+        /* navigate('/create-order'); */
+        console.log(file.current);
+        console.log(file.current.files);
+        parseFile(file.current.files[0]);
+    };
+
+    const parseFile = file => {
+        const config = {
+            header: true,
+            complete: (results,file)=> console.log(results, file)
+        }
+        PaPa.parse(file, config);
     };
 
     return (
@@ -19,11 +32,11 @@ const PopupUploadFile = (props) => {
                         <h4>Seleccionar archivo aquí</h4>
                     </header>
                     <p>Formato: CSV</p>
-                    <input type="file" accept=".csv" />
+                    <input type="file" accept=".csv" ref={file}/>
                 </div>
                 <div className='btns-popup-upload'>
                 <button className='btn-upload-popup' onClick={() => props.setPopup(false)}>Cancelar</button>
-                <button className="btn" onClick={navigateToCreateOrder}>Aceptar</button>
+                <button className="btn" onClick={handleFileUpload}>Aceptar</button>
                 </div>
             </div>
         </div>
